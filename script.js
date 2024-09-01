2985c82a0d5b65118ec47587926a973569d030ec
iamlilili <139675379+iamlilili@users.noreply.github.com>
2024-09-02 02:26:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,20 @@ Promise.all([
   faceapi.nets.faceExpressionNet.loadFromUri('./models')
 ]).then(startVideo)
 
-function startVideo() {
-    navigator.getUserMedia(
-      { video: {} },
-      stream => video.srcObject = stream,
-      err => console.error(err)
-    )
-  }
+ function startVideo() {
+        navigator.mediaDevices.getUserMedia(
+          { video: { facingMode: 'user' } } // 'user' 使用前置鏡頭, 'environment' 使用後置鏡頭
+        ).then(stream => {
+          video.srcObject = stream;
+        }).catch(err => console.error("Error accessing the camera: " + err))
+      }
 
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
+  
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
