0815c80a046092fc7e05c9e100f1cbbda56b8f52
iamlilili <139675379+iamlilili@users.noreply.github.com>
2024-09-02 02:37:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,24 +13,30 @@ const video = document.getElementById('video')
           { video: { facingMode: 'user' } } // 'user' 使用前置鏡頭, 'environment' 使用後置鏡頭
         ).then(stream => {
           video.srcObject = stream;
+          video.onloadedmetadata = () => {
+            video.play();
+            runFaceDetection();
+          };
         }).catch(err => console.error("Error accessing the camera: " + err));
       }
 
-      video.addEventListener('play', () => {
-        const canvas = faceapi.createCanvasFromMedia(video);
-        document.body.append(canvas);
-        const displaySize = { width: video.videoWidth, height: video.videoHeight };
-        faceapi.matchDimensions(canvas, displaySize);
-        
-        setInterval(async () => {
-          const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-          if (detections.length === 0) {
-            console.warn("No faces detected");
-          }
-          const resizedDetections = faceapi.resizeResults(detections, displaySize);
-          canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-          faceapi.draw.drawDetections(canvas, resizedDetections);
-          faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-          faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-        }, 100);
-      });
+       function runFaceDetection() {
+        video.addEventListener('play', () => {
+          const canvas = faceapi.createCanvasFromMedia(video);
+          document.body.append(canvas);
+          const displaySize = { width: video.videoWidth, height: video.videoHeight };
+          faceapi.matchDimensions(canvas, displaySize);
+          
+          setInterval(async () => {
+            const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+            if (detections.length === 0) {
+              console.warn("No faces detected");
+            }
+            const resizedDetections = faceapi.resizeResults(detections, displaySize);
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+            faceapi.draw.drawDetections(canvas, resizedDetections);
+            faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+            faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+          }, 100);
+        });
+      }
