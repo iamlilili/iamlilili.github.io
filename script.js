b0f17a3365e1fdadf5c2373000f972bb6a830a2c
iamlilili <139675379+iamlilili@users.noreply.github.com>
2024-09-06 10:11:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,86 @@
-const video = document.getElementById('video')
+// 全局變量
+let videoElement, canvas, faceApiCanvas, imageContainer;
+let currentFacingMode = "user";
 
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('./models')
-]).then(startVideo)
-
-function startVideo() {
-   navigator.mediaDevices.getUserMedia()(  //請求訪問鏡頭的 Web API
-      { video: {} }, // 將影片顯示在html裡的video
-      stream => video.srcObject = stream, //成功
-      err => console.error(err) //失敗
-    )
+function switchCamera() {
+  currentFacingMode = currentFacingMode === "user" ? "environment" : "user";
+  if (videoElement.srcObject) {
+    let videoTracks = videoElement.srcObject.getVideoTracks();
+    videoTracks.forEach(track => track.stop());
   }
+  startCam();
+}
 
-video.addEventListener('play', () => {   //當開始啟用攝像頭
-  const canvas = faceapi.createCanvasFromMedia(video)    //創建畫布。用來畫臉上的點點
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+function startCam() {
+  const constraints = { video: { facingMode: currentFacingMode } };
+  
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(function (stream) {
+        videoElement.srcObject = stream;
+        videoElement.setAttribute("autoplay", "");
+        videoElement.setAttribute("muted", "");
+        videoElement.setAttribute("playsinline", "");
+        
+        // 設置面部檢測
+        setupFaceDetection();
+        
+        // 設置圖像捕獲
+        setupImageCapture();
+      })
+      .catch(function (error) {
+        console.error("無法取得視訊串流:", error);
+        alert("重新開啟頁面！");
+      });
+  } else {
+    alert("您使用的瀏覽器不支援視訊串流");
+  }
+}
+
+function setupFaceDetection() {
+  faceApiCanvas = faceapi.createCanvasFromMedia(videoElement);
+  document.body.append(faceApiCanvas);
+  const displaySize = { width: videoElement.width, height: videoElement.height };
+  faceapi.matchDimensions(faceApiCanvas, displaySize);
+
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()   //使用 TinyFaceDetector 模型檢測視頻中的所有人臉，並添加面部特徵點。
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height) //清除上一幀繪製內容。
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-  }, 100)   
-})
+    const detections = await faceapi.detectAllFaces(videoElement, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    faceApiCanvas.getContext('2d').clearRect(0, 0, faceApiCanvas.width, faceApiCanvas.height);
+    faceapi.draw.drawFaceLandmarks(faceApiCanvas, resizedDetections);
+  }, 100);
+}
+
+function setupImageCapture() {
+  setInterval(function () {
+    canvas = document.createElement("canvas");
+    canvas.width = videoElement.videoWidth;
+    canvas.height = videoElement.videoHeight;
+    canvas.getContext("2d").drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+    
+    let scaleFactor = 2;
+    let offscreenCanvas = document.createElement("canvas");
+    offscreenCanvas.width = canvas.width * scaleFactor;
+    offscreenCanvas.height = canvas.height * scaleFactor;
+    let offscreenCtx = offscreenCanvas.getContext("2d");
+    offscreenCtx.drawImage(canvas, 0, 0, offscreenCanvas.width, offscreenCanvas.height);
+    
+    let imageElement = document.createElement("img");
+    imageElement.setAttribute("style", "width:100%");
+    imageContainer.innerHTML = "";
+    imageContainer.appendChild(imageElement);
+    imageElement.src = offscreenCanvas.toDataURL("image/jpeg", 1);
+  }, 1000);
+}
+
+// 初始化
+document.addEventListener('DOMContentLoaded', () => {
+  videoElement = document.getElementById('video');
+  imageContainer = document.getElementById('imageContainer');
+  
+  // 加載 face-api 模型
+  Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+  ]).then(startCam);
+});
